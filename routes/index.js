f63f9056016ec10622ae834e430e8aef070e7629
Trung <chinhtrung596@gmail.com>
2017-06-05 23:22:45 +0000

    add failureFlash for login

--- routes/index.js
+++ routes/index.js
@@ -37,7 +37,8 @@ router.get("/login",function(req,res){
 router.post("/login",passport.authenticate("local",
     {
         successRedirect: "/campgrounds",
-        failureRedirect: "/login"
+        failureRedirect: "/login",
+        failureFlash: true
     }),function(req,res){
 });
 
