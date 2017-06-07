082b8198ff771d7903711a8ba7f0d0f87931cc93
Trung <chinhtrung596@gmail.com>
2017-06-07 15:07:16 +0000

    export DATABASEURL for testing and deploying

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index")
     
-mongoose.connect("mongodb://trung:trungv1@ds161551.mlab.com:61551/yelpcampv12");
-//   mongodb://<dbuser>:<dbpassword>@ds161551.mlab.com:61551/yelpcampv12
+mongoose.connect(process.env.DATABASEURL);
+
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
