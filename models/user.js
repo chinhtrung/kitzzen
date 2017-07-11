b14dd71d773b551e8cda145bc664fb8ae4cd6492
Trung <chinhtrung596@gmail.com>
2017-07-11 03:18:06 +0000

    add user feature

--- models/user.js
+++ models/user.js
@@ -4,6 +4,9 @@ var passportLocalMongoose = require("passport-local-mongoose");
 var UserSchema = mongoose.Schema({
     username : {type: String, unique: true, required: true},
     password : String,
+    avatar: String,
+    firstName: String,
+    lastName: String,
     email: {type: String, unique: true, required: true},
     resetPasswordToken: String,
     resetPasswordExpires: Date,
