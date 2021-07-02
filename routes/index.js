aa2e991871987468bad58daa93b9dfc508f17626
chinhtrung <chinhtrung596@gmail.com>
2021-07-02 14:56:14 -0400

    update noti message

--- routes/index.js
+++ routes/index.js
@@ -61,7 +61,7 @@ router.post("/login",passport.authenticate("local",
 // logout route
 router.get("/logout",function(req,res){
     req.logout();
-    req.flash("success","See you later!");
+    req.flash("success","Success, you are logged out!");
     res.redirect("/foods");
 });
 
