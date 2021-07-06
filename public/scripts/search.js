6647ba7aa3f536f27daabef0ff4b06ed966d8faa
Trung Nguyen <chinhtrung596@gmail.com>
2021-07-06 15:35:06 -0400

    update algo method on showing food after searching

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -4,35 +4,11 @@ $('#food-search').on('input', function() {
     search = "all";
   }
   $.get('/foods?' + search, function(data) {
-    $('#food-grid').html('');
+    document.querySelectorAll('.food-post').forEach(each => each.classList.add('hidden'));
     data.forEach(function(food) {
-      $('#food-grid').append(`
-        <div class="col-md-3 col-sm-6">
-          <div class="thumbnail">
-            <img src="${ food.image }">
-            <div class="caption">
-              <h3>${ food.name }</h3>
-              <h5>
-                <strong>$ ${ food.price }</strong> by <a href="/users/${ food.author.id }"> ${ food.author.username }</a>
-              </h5>
-              <hr class="style13">
-              <p>${ food.description.slice(0,100) }...</p>
-              <p>
-                <div>
-                  <div class="ui primary labeled button" tabindex="0">
-                    <button class="ui basic primary button">
-                      <a href="/foods/${ food._id }"><i class="book icon"></i> View</a>
-                    </button>
-                    <div class="ui basic blue left pointing label">
-                      ${food.seen}
-                    </div>
-                  </div>
-                </div>
-              </p>
-            </div>
-          </div>
-        </div>
-      `);
+      let selectedFood = document.querySelector(`#food-id-${food._id}`);
+      console.dir(selectedFood);
+      selectedFood.classList.remove('hidden');
     });
   });
 });
