f3f4ccee3fc5ae5d24d5b18e1dbab9c18b4942cd
Trung Nguyen <chinhtrung596@gmail.com>
2021-07-07 20:00:15 -0400

    add delete method for rating

--- routes/ratings.js
+++ routes/ratings.js
@@ -9,23 +9,50 @@ router.post('/', middleware.isLoggedIn, middleware.checkRatingExists, function(r
 		if(err) {
 			console.log(err);
 		} else if (req.body.rating) {
-				Rating.create(req.body.rating, function(err, rating) {
-					if(err) {
-						console.log(err);
-					}
-					rating.author.id = req.user._id;
-					rating.author.username = req.user.username;
-					rating.author.avatar = req.user.avatar;
-					rating.save();
-					resultFood.ratings.push(rating);
-					resultFood.save();
-					req.flash("success", "Successfully added rating");
-				});
+			Rating.create(req.body.rating, function(err, rating) {
+				if(err) {
+					console.log(err);
+				}
+				rating.author.id = req.user._id;
+				rating.author.username = req.user.username;
+				rating.author.avatar = req.user.avatar;
+				rating.save();
+				resultFood.ratings.push(rating);
+				resultFood.save();
+				req.flash("success", "Successfully added rating");
+			});
 		} else {
 				req.flash("error", "Please select a rating");
 		}
 		res.redirect('/foods/' + resultFood._id + '#rating');
 	});
+});
+
+//RATING DESTROY ROUTE
+router.delete("/:rating_id",middleware.checkRatingOwnership,function(req,res){
+    //findByIdAndRemove
+	Food.findById(req.params.id, function(err, resultFood) {
+		if(err) {
+			console.log(err);
+		} else {
+			Rating.findByIdAndRemove(req.params.rating_id,function(err){
+				if(err){
+					req.flash("error",err.message);
+					res.redirect("back");
+				} else {
+					for (let i = 0; i < resultFood.ratings.length; i++) {
+						if (resultFood.ratings[i] == req.params.rating_id) {
+							resultFood.ratings.splice(i, 1);
+							resultFood.save();
+							break;
+						}
+					}
+					req.flash("success","Your review deleted");
+				}
+				res.redirect("/foods/" + req.params.id + "#rating");
+			});
+		}
+	})
 });
 
 module.exports = router;
