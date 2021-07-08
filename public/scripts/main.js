5ce977fdfdd594fa4954972e8ef484611b26f73d
chinhtrung <chinhtrung596@gmail.com>
2021-07-08 17:54:15 -0400

    add runEmojiPicker

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -11,3 +11,28 @@ $(document).ready(function(){
         });
     });
 });
+
+function runEmojiPicker() {
+    let tepr = document.querySelector("#trigger-emoji-picker-rating");
+    let epr = document.querySelector("#emoji-picker-rating");
+    tepr.addEventListener("click", ()=>{epr.classList.toggle("hidden")});
+
+    let epRating = document.querySelector('#emoji-picker-rating emoji-picker');
+    let textRating = document.querySelector('#textarea-rating');
+    console.dir(epr);
+    epRating.addEventListener('emoji-click', event => {
+        let emo = event.detail.unicode;
+        textRating.value += emo;
+    });
+
+    document.addEventListener('click', function(event) {
+        let isClickInsideElement = tepr.contains(event.target) || epr.contains(event.target);
+        if (!isClickInsideElement) {
+            epr.classList.add("hidden");
+        }
+    });
+}
+
+window.onload = function () {
+    runEmojiPicker();
+}
