e7122da14895d8104cbc88f3638e5915675280bc
chinhtrung <chinhtrung596@gmail.com>
2021-07-09 12:29:03 -0400

    fix bug in emoji picker logic

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -15,24 +15,26 @@ $(document).ready(function(){
 function runEmojiPicker(selector) {
     let toggleButton = document.querySelector(`#trigger-emoji-picker-${selector}`);
     let pickerContainer = document.querySelector(`#emoji-picker-${selector}`);
-    toggleButton.addEventListener("click", ()=>{pickerContainer.classList.toggle("hidden")});
-
     let selectedPicker = document.querySelector(`#emoji-picker-${selector} emoji-picker`);
     let textRating = document.querySelector(`#textarea-${selector}`);
-    selectedPicker.addEventListener('emoji-click', event => {
-        let emo = event.detail.unicode;
-        textRating.value += emo;
-    });
 
-    document.addEventListener('click', function(event) {
-        let isClickInsideElement = toggleButton.contains(event.target) || pickerContainer.contains(event.target);
-        if (!isClickInsideElement) {
-            pickerContainer.classList.add("hidden");
-        }
-    });
+    if (toggleButton && pickerContainer && selectedPicker && textRating) {
+        toggleButton.addEventListener("click", ()=>{pickerContainer.classList.toggle("hidden")});
+        selectedPicker.addEventListener('emoji-click', event => {
+            let emo = event.detail.unicode;
+            textRating.value += emo;
+        });
+
+        document.addEventListener('click', function(event) {
+            let isClickInsideElement = toggleButton.contains(event.target) || pickerContainer.contains(event.target);
+            if (!isClickInsideElement) {
+                pickerContainer.classList.add("hidden");
+            }
+        });
+    }
 }
 
 window.onload = function () {
-    runEmojiPicker("rating");
-    runEmojiPicker("comment");
+    let addEmoPicker = ["rating", "comment"];
+    addEmoPicker.forEach(each => {runEmojiPicker(each)});
 }
