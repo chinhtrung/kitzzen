9ee13226c60d546f58cff33ad4de100ff009c786
chinhtrung <chinhtrung596@gmail.com>
2021-07-10 20:49:47 -0400

    modularize from route

---
+++ controllers/ratings.js
@@ -0,0 +1,58 @@
+const Food = require("../models/food");
+const Rating = require("../models/rating");
+
+const addRating = (req, res) => {
+    Food.findById(req.params.id, (err, resultFood) => {
+        if (err) {
+            console.log(err);
+        } else if (req.body.rating) {
+            Rating.create(req.body.rating, (err, rating) => {
+                if (err) {
+                    console.log(err);
+                }
+                rating.author.id = req.user._id;
+                rating.author.username = req.user.username;
+                rating.author.avatar = req.user.avatar;
+                rating.save();
+                resultFood.ratings.push(rating);
+                resultFood.save();
+                req.flash("success", "Successfully added rating");
+            });
+        } else {
+            req.flash("error", "Please select a rating");
+        }
+        res.redirect('/foods/' + req.params.id + '#rating');
+    });
+}
+
+//RATING DESTROY ROUTE
+const deleteRating = (req, res) => {
+    //findByIdAndRemove
+    Food.findById(req.params.id, (err, resultFood) => {
+        if (err) {
+            console.log(err);
+        } else {
+            Rating.findByIdAndRemove(req.params.rating_id, (err) => {
+                if (err) {
+                    req.flash("error", err.message);
+                    res.redirect("back");
+                } else {
+                    for (let i = 0; i < resultFood.ratings.length; i++) {
+                        if (resultFood.ratings[i] == req.params.rating_id) {
+                            resultFood.ratings.splice(i, 1);
+                            resultFood.save();
+                            break;
+                        }
+                    }
+                    req.flash("success", "Your review deleted");
+                }
+                res.redirect("/foods/" + req.params.id + "#rating");
+            });
+        }
+    })
+}
+
+module.exports = {
+    addRating,
+    deleteRating
+}
