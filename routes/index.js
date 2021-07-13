3953cbc375d5590c885df78b80894ff6f40e38d8
chinhtrung <chinhtrung596@gmail.com>
2021-07-12 22:26:43 -0400

    setup cloudinary storage for user

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const middleware = require("../middleware");
+const {upload} = require("../cloudinary");
 const index = require("../controllers/index");
 
 router.get("/", index.rootRoute);
@@ -26,7 +27,7 @@ router.get('/reset/:token', index.getReset);
 router.post('/reset/:token', index.postReset);
 router.get("/users/:id", index.getUserProfile);
 router.get("/users/:id/edit", middleware.isLoggedIn, middleware.checkMatchingUser, index.editUserProfile);
-router.put("/users/:id", index.updateUserProfile);
+router.put("/users/:id", upload.single('image'), index.updateUserProfile);
 router.delete("/users/:id", middleware.checkMatchingUser, index.deleteUserAccount);
 
 module.exports = router;
