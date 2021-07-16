6c2a146563b31d6611fa31e275b1dbb1562d910c
chinhtrung <chinhtrung596@gmail.com>
2021-07-16 01:26:58 -0400

    add err handler

--- public/scripts/showPageMap.js
+++ public/scripts/showPageMap.js
@@ -1,43 +1,51 @@
-mapboxgl.accessToken = mapToken; // mapToken variable is coming from header file
-const coordinates = thisFood.geometry.coordinates;
-const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: coordinates, // starting position [lng, lat]
-    zoom: 10 // starting zoom
-});
+const setupMapToShow = () => {
+    mapboxgl.accessToken = mapToken; // mapToken variable is coming from header file
+    const coordinates = thisFood.geometry.coordinates;
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: coordinates, // starting position [lng, lat]
+        zoom: 10 // starting zoom
+    });
 
-// Create a default Marker and add it to the map.
-const marker = new mapboxgl.Marker({ color: "red" })
-    .setLngLat(coordinates)
-    .setPopup(
-        new mapboxgl.Popup({ offset: 25 })
-            .setHTML(
-                `<h4>${thisFood.name}</h4>
+    // Create a default Marker and add it to the map.
+    const marker = new mapboxgl.Marker({ color: "red" })
+        .setLngLat(coordinates)
+        .setPopup(
+            new mapboxgl.Popup({ offset: 25 })
+                .setHTML(
+                    `<h4>${thisFood.name}</h4>
                 <p>${"point anywhere near here and your location for the estimated direction"}</p>`
-            )
-    )
-    .addTo(map);
+                )
+        )
+        .addTo(map);
 
-// Add fullscreen control to the map.
-map.addControl(new mapboxgl.FullscreenControl());
+    // Add fullscreen control to the map.
+    map.addControl(new mapboxgl.FullscreenControl());
 
-// Add geolocate control to the map.
-map.addControl(
-    new mapboxgl.GeolocateControl({
-        positionOptions: {
-            enableHighAccuracy: true
-        },
-        trackUserLocation: true
-    })
-);
+    // Add geolocate control to the map.
+    map.addControl(
+        new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+            },
+            trackUserLocation: true
+        })
+    );
 
-// Add zoom and rotation controls to the map.
-map.addControl(new mapboxgl.NavigationControl());
+    // Add zoom and rotation controls to the map.
+    map.addControl(new mapboxgl.NavigationControl());
 
-map.addControl(
-    new MapboxDirections({
-        accessToken: mapboxgl.accessToken
-    }),
-    'top-left'
-);
+    map.addControl(
+        new MapboxDirections({
+            accessToken: mapboxgl.accessToken
+        }),
+        'top-left'
+    );
+}
+
+try {
+    setupMapToShow();
+} catch (e) {
+    console.log(e);
+}
