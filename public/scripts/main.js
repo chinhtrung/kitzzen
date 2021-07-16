6706370940eb0e801324f1f5f003a81203901b67
chinhtrung <chinhtrung596@gmail.com>
2021-07-16 14:27:00 -0400

    format code

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,31 +1,31 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $("#delete-confirmation").hide();
-    $('#delete-request').click(function(){
+    $('#delete-request').click(function () {
         $('#delete-confirmation').fadeToggle('fast');
     });
     $(".hidden-button").hide();
-    $('.add-reply').click(function(){
+    $('.add-reply').click(function () {
         $('.hidden-button').fadeIn('fast');
-        $('.cancel-button').click(function(){
+        $('.cancel-button').click(function () {
             $('.hidden-button').hide();
         });
     });
 });
 
-function runEmojiPickerID(selectedField) {
+const runEmojiPickerID = (selectedField) => {
     let toggleButton = document.querySelector(`#trigger-emoji-picker-${selectedField}`);
     let pickerContainer = document.querySelector(`#emoji-picker-${selectedField}`);
     let selectedPicker = document.querySelector(`#emoji-picker-${selectedField} emoji-picker`);
     let textRating = document.querySelector(`#textarea-${selectedField}`);
 
     if (toggleButton && pickerContainer && selectedPicker && textRating) {
-        toggleButton.addEventListener("click", ()=>{pickerContainer.classList.toggle("hidden")});
+        toggleButton.addEventListener("click", () => { pickerContainer.classList.toggle("hidden") });
         selectedPicker.addEventListener('emoji-click', event => {
             let emo = event.detail.unicode;
             textRating.value += emo;
         });
 
-        document.addEventListener('click', function(event) {
+        document.addEventListener('click', (event) => {
             let isClickInsideElement = toggleButton.contains(event.target) || pickerContainer.contains(event.target);
             if (!isClickInsideElement) {
                 pickerContainer.classList.add("hidden");
@@ -34,7 +34,8 @@ function runEmojiPickerID(selectedField) {
     }
 }
 
-window.onload = function () {
-    let addEmoPicker = ["rating", "comment","description"];
-    addEmoPicker.forEach(each => {runEmojiPickerID(each)});
+window.onload = () => {
+    // setup emoji picker for the following
+    let addEmoPicker = ["rating", "comment", "description"];
+    addEmoPicker.forEach(each => { runEmojiPickerID(each) });
 }
