b623dc19f72d09e60d5199f0e177ed3f744a141f
chinhtrung <chinhtrung596@gmail.com>
2021-07-25 14:28:50 -0400

    format code

--- app.js
+++ app.js
@@ -73,6 +73,7 @@ app.get("/*", (req, res) => {
 app.use((req, res, next) => {
     res.status(404).send("Sorry can't find that!");
 });
+
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
