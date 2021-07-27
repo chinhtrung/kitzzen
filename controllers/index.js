698015faa498b08d69810f42f51358ae8c586ace
chinhtrung <chinhtrung596@gmail.com>
2021-07-27 17:47:35 -0400

    add error handler

--- controllers/index.js
+++ controllers/index.js
@@ -7,6 +7,12 @@ const passport = require("passport");
 const nodemailer = require("nodemailer");
 const crypto = require("crypto");
 const { cloudinary } = require("../cloudinary");
+const path = require('path');
+const scriptName = path.dirname(__filename) + "/" + path.basename(__filename);
+
+const errorMessageTryCatch = (err) => {
+    console.log(errorHandler.errorMessage(err, scriptName));
+}
 
 // root route
 const rootRoute = (req, res) => {
@@ -219,25 +225,30 @@ const updateUserProfile = async (req, res) => {
     if (req.body.isadmin === process.env.ADMIN_CODE) {
         isAdmin = true;
     }
-    
+
     let avatar = req.body.prevAvatar;
     let cloudinaryID = req.body.cloudinaryID;
 
-    await User.findById(req.params.id, (err, user) => {
-        // prevent user change avatar without adding new picture
-        avatar = user.avatar;
-    });
+    try {
+        await User.findById(req.params.id, (err, user) => {
+            // prevent user change avatar without adding new picture
+            avatar = user.avatar;
+        });
+    } catch (err) { errorMessageTryCatch(err); }
 
     if (req.file) {
         // Delete avatar from cloudinary
         try {
             await cloudinary.uploader.destroy(cloudinaryID);
-        } catch (err) { console.log(err) }
+        } catch (err) { errorMessageTryCatch(err); }
+
         // Upload avatar to cloudinary
-        await cloudinary.uploader.upload(req.file.path, (result) => {
-            avatar = result.secure_url;
-            cloudinaryID = result.public_id;
-        });
+        try {
+            await cloudinary.uploader.upload(req.file.path, (result) => {
+                avatar = result.secure_url;
+                cloudinaryID = result.public_id;
+            });
+        } catch (err) { errorMessageTryCatch(err); }
     }
 
     User.findByIdAndUpdate(req.params.id, {
@@ -262,21 +273,29 @@ const updateUserProfile = async (req, res) => {
     // Update author avatar of food
     Food.find().where('author.id').equals(req.params.id).exec((err, resultFoods) => {
         resultFoods.forEach(async (food) => {
-            await Food.findByIdAndUpdate(food._id, {'author.avatar' : avatar});
+            try {
+                await Food.findByIdAndUpdate(food._id, { 'author.avatar': avatar });
+            } catch (err) { errorMessageTryCatch(err); }
         });
     });
 
     // Update comment avatar of food
     Comment.find().where('author.id').equals(req.params.id).exec((err, resultComments) => {
         resultComments.forEach(async (comment) => {
-            await Comment.findByIdAndUpdate(comment._id, {'author.avatar' : avatar});
+            try {
+                await Comment.findByIdAndUpdate(comment._id, { 'author.avatar': avatar });
+            } catch (err) { errorMessageTryCatch(err); }
+
         });
     });
 
     // Update rating avatar of food
     Rating.find().where('author.id').equals(req.params.id).exec((err, resultRatings) => {
         resultRatings.forEach(async (rating) => {
-            await Rating.findByIdAndUpdate(rating._id, {'author.avatar' : avatar});
+            try {
+                await Rating.findByIdAndUpdate(rating._id, { 'author.avatar': avatar });
+            } catch (err) { errorMessageTryCatch(err); }
+
         });
     });
 }
@@ -290,24 +309,31 @@ const deleteUserAccount = async (req, res) => {
                 // Delete image from cloudinary
                 try {
                     await cloudinary.uploader.destroy(food.cloudinaryID);
-                } catch (err) { console.log(err) }
+                } catch (err) { errorMessageTryCatch(err); }
 
                 // Delete ratings of this food in database
                 food.ratings.forEach(async rating => {
-                    await Rating.findByIdAndRemove(rating._id);
+                    try {
+                        await Rating.findByIdAndRemove(rating._id);
+                    } catch (err) { errorMessageTryCatch(err); }
                 });
 
                 // Delete comments of this food in database
                 food.comments.forEach(async comment => {
-                    await Comment.findByIdAndRemove(comment._id);
+                    try {
+                        await Comment.findByIdAndRemove(comment._id);
+                    } catch (err) { errorMessageTryCatch(err); }
                 });
-                
-                await Food.findByIdAndRemove(food._id, (err) => {
-                    if (err) {
-                        req.flash("error", err.message);
-                        res.redirect("/foods");
-                    }
-                });
+
+                // Delete Food from this user
+                try {
+                    await Food.findByIdAndRemove(food._id, (err) => {
+                        if (err) {
+                            req.flash("error", err.message);
+                            res.redirect("/foods");
+                        }
+                    });
+                } catch (err) { errorMessageTryCatch(err); }
             });
         });
 
@@ -318,45 +344,59 @@ const deleteUserAccount = async (req, res) => {
                 for (let i = 0; i < food.ratings.length; i++) {
                     let rating = food.ratings[i];
                     let authorId = undefined;
-                    await Rating.findById(rating, "author.id").exec().then(value => {
-                        authorId = value? value.author.id : undefined;
-                    });
-
+                    try {
+                        await Rating.findById(rating, "author.id").exec().then(value => {
+                            authorId = value ? value.author.id : undefined;
+                        });
+                    } catch (err) { errorMessageTryCatch(err); }
+                    
                     if (authorId && authorId.equals(req.params.id)) {
                         food.ratings.splice(i, 1);
-                        await food.save();
+                        try {
+                            await food.save();
+                        } catch (err) { errorMessageTryCatch(err); }
                     }
 
                     // clean up undefined element in food.ratings array if there is any
                     if (!authorId) {
                         food.ratings.splice(i, 1);
-                        await food.save();
+                        try {
+                            await food.save();
+                        } catch (err) { errorMessageTryCatch(err); }
                         i--; // to iteratively delete all matching element in array
                     }
 
                     // recaculate rating
-                    let avgRating = food.ratings.length == 0? 0: food.ratings.reduce((acc, each) => acc + each.rating, 0) / food.ratings.length;
-                    await Food.findByIdAndUpdate(food._id, {rating: avgRating? avgRating : 0});
+                    let avgRating = food.ratings.length == 0 ? 0 : food.ratings.reduce((acc, each) => acc + each.rating, 0) / food.ratings.length;
+                    try {
+                        await Food.findByIdAndUpdate(food._id, { rating: avgRating ? avgRating : 0 });
+                    } catch (err) { errorMessageTryCatch(err); }
                 }
-                
+
                 // check comments
                 for (let i = 0; i < food.comments.length; i++) {
                     let comment = food.comments[i];
                     let authorId = undefined;
-                    await Comment.findById(comment, "author.id").exec().then(value => {
-                        authorId = value? value.author.id : undefined;
-                    });
-
+                    try {
+                        await Comment.findById(comment, "author.id").exec().then(value => {
+                            authorId = value ? value.author.id : undefined;
+                        });
+                    } catch (err) { errorMessageTryCatch(err); }
+                    
                     if (authorId && authorId.equals(req.params.id)) {
                         food.comments.splice(i, 1);
-                        await food.save();
+                        try {
+                            await food.save();
+                        } catch (err) { errorMessageTryCatch(err); }
                         i--; // to iteratively delete all matching element in array
                     }
 
                     // clean up undefined element in food.comments array if there is any
                     if (!authorId) {
                         food.comments.splice(i, 1);
-                        await food.save();
+                        try {
+                            await food.save();
+                        } catch (err) { errorMessageTryCatch(err); }
                         i--; // to iteratively delete all matching element in array
                     }
                 }
@@ -366,7 +406,9 @@ const deleteUserAccount = async (req, res) => {
                     let yum = food.yums[i];
                     if (yum.equals(req.params.id)) {
                         food.yums.splice(i, 1);
-                        await food.save()
+                        try {
+                            await food.save();
+                        } catch (err) { errorMessageTryCatch(err); }
                     }
                 }
             })
@@ -375,33 +417,37 @@ const deleteUserAccount = async (req, res) => {
         // Delete comment associated with this user
         Comment.find().where('author.id').equals(req.params.id).exec((err, resultComments) => {
             resultComments.forEach(async (comment) => {
-                await Comment.findByIdAndRemove(comment._id, (err) => {
-                    if (err) {
-                        req.flash("error", err.message);
-                        res.redirect("/foods");
-                    }
-                });
+                try {
+                    await Comment.findByIdAndRemove(comment._id, (err) => {
+                        if (err) {
+                            req.flash("error", err.message);
+                            res.redirect("/foods");
+                        }
+                    });
+                } catch (err) { errorMessageTryCatch(err); }
             });
         });
 
         // Delete rating associated with this user
         Rating.find().where('author.id').equals(req.params.id).exec((err, resultRatings) => {
             resultRatings.forEach(async (rating) => {
-                await Rating.findByIdAndRemove(rating._id, (err) => {
-                    if (err) {
-                        req.flash("error", err.message);
-                        res.redirect("/foods");
-                    }
-                });
+                try {
+                    await Rating.findByIdAndRemove(rating._id, (err) => {
+                        if (err) {
+                            req.flash("error", err.message);
+                            res.redirect("/foods");
+                        }
+                    });
+                } catch (err) { errorMessageTryCatch(err); }
             });
         });
-        
+
         // Delete this user
         User.findByIdAndRemove(req.params.id, async (err, resultUser) => {
             // Delete image from cloudinary
             try {
                 await cloudinary.uploader.destroy(resultUser.cloudinaryID);
-            } catch (err) { console.log(err) }
+            } catch (err) { errorMessageTryCatch(err); }
 
             if (err) {
                 req.flash("error", err.message);
