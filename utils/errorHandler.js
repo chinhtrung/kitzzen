a43439f2b0093787b4d5f7f91d91194b31008b1f
chinhtrung <chinhtrung596@gmail.com>
2021-07-31 04:29:24 -0400

    add errorResponse

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,7 +1,14 @@
 const errorMessage = (err, path) => {
-    return `!!!!!!!!!! ERROR: ${err} OCCURED AT ${path}`;
+    console.log(`!!!!!!!!!! ERROR: ${err} OCCURED AT ${path}`);
+}
+
+const errorResponse = (req, res, err, path) => {
+    errorMessage(err, path);
+    req.flash("error", "error in errorHandler");
+    res.redirect("/error");
 }
 
 module.exports = {
-    errorMessage
+    errorMessage,
+    errorResponse
 }
